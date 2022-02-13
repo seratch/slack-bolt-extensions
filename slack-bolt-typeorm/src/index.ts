9159fc3ac52068d1815a30b35f74dae1cffd7351
Kazuhiro Sera <seratch@gmail.com>
2022-02-13 09:46:15 +0900

    Fix

--- slack-bolt-typeorm/src/index.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-internal-modules */
-/* eslint-disable import/extensions */
-
-export { default as InstallationEntity } from './InstallationEntity';
-export { default as TypeORMInstallationStore } from './TypeORMINstallationStore';
-export { default as TypeORMINstallationStoreArgs } from './TypeORMINstallationStoreArgs';
