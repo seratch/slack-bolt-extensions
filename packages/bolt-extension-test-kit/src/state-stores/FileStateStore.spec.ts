9eeb651a7dadd4d6acf858b0670e2fbce550b7e3
Kazuhiro Sera <seratch@gmail.com>
2022-02-28 23:07:26 +0900

    Simplify FileStateStore

--- packages/bolt-extension-test-kit/src/state-stores/FileStateStore.spec.ts
+++ packages/bolt-extension-test-kit/src/state-stores/FileStateStore.spec.ts
@@ -4,7 +4,6 @@ import { StateStoreChaiTestRunner } from './StateStoreChaiTestRunner';
 
 const testRunner = new StateStoreChaiTestRunner({
   stateStore: new FileStateStore({
-    stateSecret: 'secret',
     baseDir: os.tmpdir(),
   }),
 });
--- packages/bolt-extension-test-kit/src/state-stores/FileStateStore.ts
+++ packages/bolt-extension-test-kit/src/state-stores/FileStateStore.ts
@@ -1,12 +1,11 @@
-import { sign, verify } from 'jsonwebtoken';
 import { InstallURLOptions, StateStore } from '@slack/oauth';
 import { homedir } from 'os';
 import fs from 'fs';
 import path from 'path';
+import { randomUUID } from 'crypto';
 import { InvalidStateError, StateObj } from '../workaround';
 
 export interface FileStateStoreArgs {
-  stateSecret: string;
   stateExpirationSeconds?: number;
   baseDir?: string;
 }
@@ -14,15 +13,12 @@ export interface FileStateStoreArgs {
 export default class FileStateStore implements StateStore {
   private baseDir: string;
 
-  private stateSecret: string;
-
   private stateExpirationSeconds: number;
 
   public constructor(args: FileStateStoreArgs) {
     this.baseDir = args.baseDir !== undefined ?
       args.baseDir :
       `${homedir()}/.bolt-js-oauth-states`;
-    this.stateSecret = args.stateSecret;
     this.stateExpirationSeconds = args.stateExpirationSeconds !== undefined ?
       args.stateExpirationSeconds :
       600;
@@ -32,13 +28,13 @@ export default class FileStateStore implements StateStore {
     installOptions: InstallURLOptions,
     now: Date,
   ): Promise<string> {
-    const source = {
+    const state = randomUUID();
+    const source: StateObj = {
       installOptions,
-      now: now.toJSON(),
+      now,
       random: Math.floor(Math.random() * 1000000),
     };
-    const state = sign(source, this.stateSecret);
-    this.writeToFile(state);
+    this.writeToFile(state, source);
     return state;
   }
 
@@ -49,23 +45,25 @@ export default class FileStateStore implements StateStore {
     try {
       if (this.findFile(state)) {
         // decode the state using the secret
-        let decoded: StateObj;
+        let decoded: StateObj | undefined;
         try {
-          decoded = verify(state, this.stateSecret) as StateObj;
+          decoded = this.readFile(state);
         } catch (e) {
           const message = `Failed to load the data represented by the state parameter (error: ${e})`;
           throw new InvalidStateError(message);
         }
-        // Check if the state value is not too old
-        const generatedAt = new Date(decoded.now);
-        const passedSeconds = Math.floor(
-          (now.getTime() - generatedAt.getTime()) / 1000,
-        );
-        if (passedSeconds > this.stateExpirationSeconds) {
-          throw new InvalidStateError('The state value is already expired');
+        if (decoded !== undefined) {
+          // Check if the state value is not too old
+          const generatedAt = new Date(decoded.now);
+          const passedSeconds = Math.floor(
+            (now.getTime() - generatedAt.getTime()) / 1000,
+          );
+          if (passedSeconds > this.stateExpirationSeconds) {
+            throw new InvalidStateError('The state value is already expired');
+          }
+          // return installOptions
+          return decoded.installOptions;
         }
-        // return installOptions
-        return decoded.installOptions;
       }
     } finally {
       this.deleteFile(state);
@@ -73,79 +71,34 @@ export default class FileStateStore implements StateStore {
     throw new InvalidStateError('The state value is already expired');
   }
 
-  private writeToFile(data: string): void {
+  private writeToFile(filename: string, data: StateObj): void {
     fs.mkdirSync(this.baseDir, { recursive: true });
-    // To make the scan faster without any additional dependencies
-    const indexPath = path.resolve(`${this.baseDir}/index`);
+    const fullpath = path.resolve(`${this.baseDir}/${filename}`);
+    fs.writeFileSync(fullpath, JSON.stringify(data));
+  }
 
-    // Use as a short name as possible to avoid
-    // "Error: ENAMETOOLONG: name too long, open" error
-    let filenameLength = 1;
-    while (filenameLength < data.length) {
-      const filename = toFilename(data, filenameLength);
-      const fullpath = path.resolve(`${this.baseDir}/${filename}`);
-      if (!fs.existsSync(fullpath)) {
-        fs.writeFileSync(fullpath, data);
-        fs.appendFileSync(indexPath, `${filename}\n`);
-        return;
+  private findFile(filename: string): boolean {
+    const fullpath = path.resolve(`${this.baseDir}/${filename}`);
+    return fs.existsSync(fullpath);
+  }
+
+  private readFile(filename: string): StateObj | undefined {
+    const fullpath = path.resolve(`${this.baseDir}/${filename}`);
+    try {
+      const data = fs.readFileSync(fullpath);
+      if (data !== undefined) {
+        return JSON.parse(data.toString());
       }
-      filenameLength += 1;
+      return undefined;
+    } catch (_) {
+      return undefined;
     }
   }
 
-  private findFile(data: string): boolean {
-    // To make the scan faster without any additional dependencies
-    const indexPath = path.resolve(`${this.baseDir}/index`);
-    const list = fs.readFileSync(indexPath).toString();
-    let found = false;
-    const filenames = list
-      .split('\n')
-      .filter((f) => f.length !== 0 && data.endsWith(f));
-    filenames.forEach((filename) => {
-      if (!found) {
-        const fullpath = path.resolve(`${this.baseDir}/${filename}`);
-        if (fs.existsSync(fullpath)) {
-          try {
-            const content = fs.readFileSync(fullpath).toString();
-            found = content === data;
-          } catch (_) {
-            // ignored
-          }
-        }
-      }
-    });
-    return found;
-  }
-
-  private deleteFile(data: string): void {
-    if (this.findFile(data)) {
-      // To make the scan faster without any additional dependencies
-      const indexPath = path.resolve(`${this.baseDir}/index`);
-      // Use as a short name as possible to avoid
-      // "Error: ENAMETOOLONG: name too long, open" error
-      let filenameLength = 1;
-      while (filenameLength < data.length) {
-        const filename = toFilename(data, filenameLength);
-        const fullpath = path.resolve(`${this.baseDir}/${filename}`);
-        if (fs.existsSync(fullpath)) {
-          fs.unlinkSync(fullpath);
-          fs.writeFileSync(
-            indexPath,
-            fs
-              .readFileSync(indexPath)
-              .toString()
-              .split('\n')
-              .filter((f) => f !== filename)
-              .join('\n'),
-          );
-        }
-        filenameLength += 1;
-      }
+  private deleteFile(filename: string): void {
+    const fullpath = path.resolve(`${this.baseDir}/${filename}`);
+    if (fs.existsSync(fullpath)) {
+      fs.unlinkSync(fullpath);
     }
   }
-}
-
-function toFilename(data: string, length: number): string {
-  const fullLength = data.length;
-  return data.substring(fullLength - length);
 }
